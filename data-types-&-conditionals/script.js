088cb6d9c86846089e1674c85abd5aaaeb69ea6e
rok1sa <139523036+rok1sa@users.noreply.github.com>
2024-12-23 22:18:53 -0800

    string section done

---
+++ data-types-&-conditionals/script.js
@@ -0,0 +1,72 @@
+// alert( 1 / 0); // Infinity
+// alert( Infinity ); // Infinity
+
+// let isGreater = 4 > 1;
+// alert( isGreater ); // true (the comparison result is "yes")
+
+const button = document.querySelector("button");
+
+function greet() {
+  const name = prompt("What is your name?");
+  const greeting = document.querySelector("#greeting");
+  greeting.textContent = `Hello ${name}, nice to see you!`;
+}
+
+button.addEventListener("click", greet);
+
+const bigmouth = 'I\'ve got no right to take my place…';
+console.log(bigmouth);
+
+/**
+ * ===== Troubleshooting =====
+ * The function below should log the number 2, however it does not, 
+ * see if you can fix it!
+ * Be sure to fix it in the spirit of the code, do not hard code the result.
+ */
+
+function troubleshooting() {
+	const a = 1;
+	const b = 1;
+
+	let result;
+
+	// Edit between these lines
+	// =================================
+	result = a + b;
+
+	// =================================
+
+	return result;
+}
+
+/**
+ * The code below tells the browser to ask you for a number
+ * then if that number is `6`, it returns `true` otherwise it returns `false`
+ * 
+ * Change this code so it returns `true` when the number is greater than or equal to 10, and false if it is less than 10
+ */
+
+number = Number(prompt("enter a number"));
+
+function numberChecker() {
+	if(number >= 10) {
+		return true;
+	} else{(number < 10)
+		return false;
+	}
+}
+
+/**
+ * Lets do some math!
+ * Some rules first:
+ *   - Replace the strings to the right of the = with the math expression they describe.
+ *   - Do not manually enter the answers to the equations. For example, `const a = 9` would be incorrect as 9 is the answer to the equation you're being asked to write out
+ */
+
+const a = 1 + 8;
+const b = 22 * 3;
+const c = 5 % 4;
+const d = a - 17;
+const e = a + b + c + d;
+
+console.log(a, b, c, d, e);
