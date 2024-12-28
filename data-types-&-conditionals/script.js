69a942e516291cb89caad6d465465a8087e52ef5
rok1sa <139523036+danny-codes@users.noreply.github.com>
2024-12-28 12:29:24 -0800

    conditionals, operators, exercises

--- data-types-&-conditionals/script.js
+++ data-types-&-conditionals/script.js
@@ -276,5 +276,4 @@ let result = (a + b < 4) ? 'Below': 'Over';
 
 let message = (login == 'Employe') ? 'Hello' :
 (login == 'Director') ?
-'Greetings' :(login == '') ?
-'No login': '';
+'Greetings' :(login == '') ?'No login': '';
