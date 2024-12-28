5b398c7a3d06f114232db72d419847eb47e215db
rok1sa <139523036+danny-codes@users.noreply.github.com>
2024-12-28 13:43:27 -0800

    switch exercise

--- data-types-&-conditionals/script.js
+++ data-types-&-conditionals/script.js
@@ -283,3 +283,32 @@ let message = (login == 'Employe') ? 'Hello' :
 'Greetings' :(login == '') ?'No login': '';
 
 console.log(message);
+
+switch (browser) {
+  case "Edge":
+    alert("You've got the Edge!");
+    break;
+
+  case "Chrome":
+  case "Firefox":
+  case "Safari":
+  case "Opera":
+    alert("Okay we support these browsers too");
+    break;
+
+  default:
+    alert("We hope that this page looks ok!");
+}
+
+browser = prompt('What browser are you currently using?');
+
+if (browser === 'Edge'){
+	console.log("You've got the Edge!")
+}
+else if (browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari'
+	|| browser == 'Opera'){
+		console.log("Okay we support these browsers too")
+	}
+else {
+	console.log('We hope this page looks okay!')
+}
