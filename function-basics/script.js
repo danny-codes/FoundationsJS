b2a845150beccc0bf32446f829da3df748046526
rok1sa <139523036+danny-codes@users.noreply.github.com>
2024-12-30 21:20:15 -0800

    functions

---
+++ function-basics/script.js
@@ -0,0 +1,17 @@
+function sum(param1, param2) {
+    return param1 + param2;
+}
+
+console.log(sum(1, 2));
+
+function favColor(color){
+    return color + ' is my favorite color'
+}
+
+console.log(favColor('Black'));
+
+function favoriteAnimal(animal) {
+    return animal + " is my favorite animal!"
+}
+
+console.log(favoriteAnimal('Whale'))
