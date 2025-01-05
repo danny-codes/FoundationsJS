21c72b6413621d28f70021fa536fe68854609ed4
rok1sa <139523036+danny-codes@users.noreply.github.com>
2025-01-04 21:45:01 -0800

    create function and variable for computer and human choice
    to get computer and user input and have initial score value

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,37 @@
+function getRandomInt() {
+    let num = 3;
+    return Math.floor(Math.random() * num);
+}
+
+function getComputerChoice() {
+    value = getRandomInt();
+    if (value === 0) {
+        return 'rock';
+    }
+    else if (value === 1) {
+        return 'paper';
+    }
+    else if (value === 2) {
+        return 'scissors';
+    }
+}
+
+function getHumanChoice() {
+    let userChoice = prompt('Choose one: rock, paper or scissors?');
+
+    if (userChoice === 'Rock' || userChoice === 'rock') {
+        return userChoice;
+    }
+    else if (userChoice === 'Paper' || userChoice === 'paper') {
+        return userChoice;
+    }
+    if (userChoice === 'Scissors' || userChoice === 'scissors') {
+        return userChoice;
+    }
+    else {
+        console.log('Sorry, not a valid input.')
+    }
+}
+
+let computerScore = 0;
+let humanScore = 0;
