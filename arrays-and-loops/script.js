af6ad98ed54d133ca93d42bc0951ce2d69da9b84
rok1sa <139523036+danny-codes@users.noreply.github.com>
2025-01-10 19:53:39 -0800

    access the full array bu referring to its name

--- arrays-and-loops/script.js
+++ arrays-and-loops/script.js
@@ -6,3 +6,4 @@ fruits[1] = 'Pineapple';
 let fruit = fruits[1];
 fruits[0] = 'Mango';
 document.getElementById('para').innerHTML = fruits.toString();
+document.getElementById('para').innerHTML = myFavNumbers;
