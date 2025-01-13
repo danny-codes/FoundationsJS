f884a3e30437a6b89ee5c9c1edc366f15fe7297c
rok1sa <139523036+danny-codes@users.noreply.github.com>
2025-01-12 19:12:33 -0800

    exiting loops with break example

--- arrays-and-loops/script.js
+++ arrays-and-loops/script.js
@@ -120,3 +120,32 @@ for (let i = 0; i < cats.length; i++) {
 }
 
 console.log(myFavoriteCats);
+
+const contacts = [
+    "Chris:2232322",
+    "Sarah:3453456",
+    "Bill:7654322",
+    "Mary:9998769",
+    "Dianne:9384975",
+];
+const para = document.getElementById("display");
+const input = document.querySelector("input");
+const btn = document.getElementById("searchBtn");
+
+btn.addEventListener("click", () => {
+    const searchName = input.value.toLowerCase();
+    input.value = "";
+    input.focus();
+    para.textContent = "";
+    for (const contact of contacts) {
+        const splitContact = contact.split(":");
+        if (splitContact[0].toLowerCase() === searchName) {
+        para.textContent = `${splitContact[0]}'s number is ${splitContact[1]}.`;
+        break;
+    }
+    }
+    if (para.textContent === "") {
+        para.textContent = "Contact not found.";
+    }
+});
+    
