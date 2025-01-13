701dd071f500a3579ab70b058d159d2fcfbf83d1
rok1sa <139523036+danny-codes@users.noreply.github.com>
2025-01-12 19:39:30 -0800

    launch countdown exercise

--- arrays-and-loops/script.js
+++ arrays-and-loops/script.js
@@ -166,3 +166,23 @@ intBtn.addEventListener("click", () => {
     par.textContent += `${i} `;
     }
 });
+
+const output = document.querySelector('.output');
+output.textContent = "";
+
+let i = 10;
+
+while (i >= 0) {
+    const para = document.createElement('p');
+    if (i === 10) {
+        para.textContent = `Countdown ${i}`;
+    } else if (i === 0) {
+        para.textContent = 'Blast off!';
+    } else {
+        para.textContent = i;
+    }
+
+    output.appendChild(para);
+
+    i--;
+}
