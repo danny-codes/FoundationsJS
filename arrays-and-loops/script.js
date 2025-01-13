3258ab15669a21e6cec0ce8b46a2d8a393284a52
rok1sa <139523036+danny-codes@users.noreply.github.com>
2025-01-12 18:55:42 -0800

    calculating squares example

--- arrays-and-loops/script.js
+++ arrays-and-loops/script.js
@@ -88,6 +88,18 @@ console.log(upperCats);
 const filtered = cats.filter((cat) => cat.startsWith('L'));
 console.log(filtered);
 
-// for (initializer; condition; final-expression) {
-//     // code to run
-// }
+const results = document.querySelector('#results');
+
+function calculate() {
+    for (let i = 1; i < 10; i++) {
+        const newResult = `${i} x ${i} = ${i * i}`;
+        results.textContent += `${newResult}\n`;
+    }
+    results.textContent += "\nFinished!\n\n";
+}
+
+const calculateBtn = document.querySelector('#calculate');
+const clearBtn = document.querySelector('#clear');
+
+calculateBtn.addEventListener('click', calculate);
+clearBtn.addEventListener('click', () => (results.textContent = ''));
