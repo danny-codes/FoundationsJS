89d7646ea677f44face64c161d9d50ed819fa2ed
rok1sa <139523036+danny-codes@users.noreply.github.com>
2025-01-16 22:04:53 -0800

    using named functions with addEventListener

--- DOM-manipulation-&-events/events.js
+++ DOM-manipulation-&-events/events.js
@@ -1,6 +1,12 @@
 const btn = document.querySelector('#btn');
 // btn.onclick = () => alert('Hello World');
 
-btn.addEventListener('click', () => {
-    alert('Hello World');
-})
+// btn.addEventListener('click', () => {
+//     alert('Hello World');
+// })
+
+function alertFunction() {
+    alert('Hey you did it!');
+}
+
+btn.addEventListener('click', alertFunction);
