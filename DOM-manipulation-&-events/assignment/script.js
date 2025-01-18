55c0b5d6757f03910b649dc13e072a5443297173
rok1sa <139523036+danny-codes@users.noreply.github.com>
2025-01-17 19:19:02 -0800

    created paragraph, added text content and appended it to a section

--- DOM-manipulation-&-events/assignment/script.js
+++ DOM-manipulation-&-events/assignment/script.js
@@ -3,3 +3,7 @@ link.textContent = 'Our Homepage';
 link.href = '';
 
 const sect = document.querySelector('section');
+
+const para = document.createElement('p');
+para.textContent = 'I am a very interesting paragraph.';
+sect.appendChild(para);
