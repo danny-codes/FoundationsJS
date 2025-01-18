36299c5c18bbe4d4169bdec39e00bf710ad928fa
rok1sa <139523036+danny-codes@users.noreply.github.com>
2025-01-17 20:54:33 -0800

    detect mouse clicks with a button click

--- DOM-manipulation-&-events/mouse-events/script.js
+++ DOM-manipulation-&-events/mouse-events/script.js
@@ -22,3 +22,35 @@ myDiv.addEventListener('dblclick', () => {
 // ...
 //  later, no longer use
 // element.onmousemove = null;
+
+let btn = document.querySelector('#btn');
+
+let body = document.querySelector('body');
+
+btn.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+});
+
+btn.addEventListener('mouseup', (e) => {
+    let msg = document.createElement('p');
+    body.appendChild(msg);
+    switch (e.button) {
+        case 0:
+            msg.textContent = 'Left mouse button clicked';
+            break;
+        case 1:
+            msg.textContent = 'Middle mouse button clicked';
+            break;
+        case 2:
+            msg.textContent = 'Right mouse button clicked';
+            break;
+        case 3:
+            msg.textContent = 'Browser back mouse button clicked';
+            break;
+        case 4:
+            msg.textContent = 'Browser forward mouse button clicked';
+            break;
+        default:
+            msg.textContent = `Unknown mouse button code ${event.button}`;
+    }
+});
