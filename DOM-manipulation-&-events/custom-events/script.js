b0b524483f0dcfc765f76b0f6d2e8dcd737a7d1e
rok1sa <139523036+danny-codes@users.noreply.github.com>
2025-01-18 20:05:41 -0800

    custom events

--- DOM-manipulation-&-events/custom-events/script.js
+++ DOM-manipulation-&-events/custom-events/script.js
@@ -1,6 +1,15 @@
-
-
-function highlight(elem) {
+function highlight(elem, callback) {
     const bgColor = 'yellow';
     elem.style.backgroundColor = bgColor;
+
+    if (callback && typeof callback === 'function') {
+        callback(elem);
+    }
 }
+
+let note = document.querySelector('.note');
+function addBorder(elem) {
+    elem.style.border = "solid 1px red";
+}
+
+highlight(note, addBorder);
