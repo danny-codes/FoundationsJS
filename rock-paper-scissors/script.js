0ad569a7ad9c4a6737e1003a0ae98fbc9b004860
daniel <139523036+danny-codes@users.noreply.github.com>
2025-01-21 20:16:17 -0800

    made a func to append results to the div

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -2,11 +2,19 @@ let rockBtn = document.querySelector('#rock');
 let paperBtn = document.querySelector('#paper');
 let scissorsBtn = document.querySelector('#scissors');
 let playAgain = document.querySelector('#playAgain');
+let results = document.querySelector('#results');
 
 let computerScore = 0;
 let humanScore = 0;
 
 let roundCount = 0;
+
+function appendMessage(message) {
+    // results.textContent += message + '\n'; doesnt work as intended
+    let messageP = document.createElement('p');
+    messageP.textContent = message;
+    results.appendChild(messageP);
+}
 
 function getRandomInt() {
     let num = 3;
@@ -29,24 +37,24 @@ function getComputerChoice() {
 function playRound(computerChoice, humanChoice) {
     if (computerChoice === humanChoice) {
         if (computerChoice === humanChoice) {
-            console.log('Tie!');
+            appendMessage('Tie!');
         }
     }
     else if (computerChoice === 'paper' && humanChoice === 'rock'
     ) {
-        console.log('You win!');
+        appendMessage('You win!');
         humanScore += 1;
     }
     else if (computerChoice === 'paper' && humanChoice === 'scissors') {
-        console.log('You win!');
+        appendMessage('You win!');
         humanScore += 1;
     }
     else if (computerChoice === 'scissors' && humanChoice === 'rock') {
-        console.log('You win!');
+        appendMessage('You win!');
         humanScore += 1;
     }
     else {
-        console.log('You lose!');
+        appendMessage('You lose!');
         computerScore += 1;
     }
 
@@ -57,15 +65,15 @@ function playRound(computerChoice, humanChoice) {
 }
 
 function endGame() {
-    console.log(`Final scores: Player ${humanScore} - Computer ${computerScore}`);
+    appendMessage(`Final scores: Player ${humanScore} - Computer ${computerScore}`);
     if (humanScore > computerScore) {
-        console.log('Congratulations, you win the game!');
+        appendMessage('Congratulations, you win the game!');
     }
     else if (humanScore < computerScore) {
-        console.log('Sorry, the computer wins the game!');
+        appendMessage('Sorry, the computer wins the game!');
     }
     else {
-        console.log("It's a tie!");
+        appendMessage("It's a tie!");
     }
 }
     
@@ -75,7 +83,7 @@ function playGame() {
             if (roundCount < 5) {
                 let humanChoice = rockBtn.id;
                 let computerSelection = getComputerChoice();
-                console.log(`Round ${roundCount + 1}: Player chose ${humanChoice}, Computer chose ${computerSelection}`);
+                appendMessage(`Round ${roundCount + 1}: Player chose ${humanChoice}, Computer chose ${computerSelection}`);
                 playRound(computerSelection, humanChoice);
             }
         });
@@ -84,7 +92,7 @@ function playGame() {
             if (roundCount < 5) {
                 let humanChoice = paperBtn.id;
                 let computerSelection = getComputerChoice();
-                console.log(`Round ${roundCount + 1}: Player chose ${humanChoice}, Computer chose ${computerSelection}`);
+                appendMessage(`Round ${roundCount + 1}: Player chose ${humanChoice}, Computer chose ${computerSelection}`);
                 playRound(computerSelection, humanChoice);
             }
         });
@@ -93,7 +101,7 @@ function playGame() {
             if (roundCount < 5) {
                 let humanChoice = scissorsBtn.id;
                 let computerSelection = getComputerChoice();
-                console.log(`Round ${roundCount + 1}: Player chose ${humanChoice}, Computer chose ${computerSelection}`);
+                appendMessage(`Round ${roundCount + 1}: Player chose ${humanChoice}, Computer chose ${computerSelection}`);
                 playRound(computerSelection, humanChoice);
             }
         });
