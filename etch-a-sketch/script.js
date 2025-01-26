0ecd835adc33abebcfbbe058935a4ededebd0e43
daniel <139523036+danny-codes@users.noreply.github.com>
2025-01-25 20:12:12 -0800

    created const for a btn, function to create a grid, event listener to get user input from a prompt

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,15 +1,27 @@
 const parentContainer = document.querySelector('#container');
 
+const numberBtn = document.querySelector('#numberBtn');
+
 const fragment = document.createDocumentFragment();
 
-for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    // div.textContent = `Div ${i + 1}`;
-    div.style.width = '100px';
-    div.style.height = '100px';
-    div.style.textAlign = 'center'
-    div.style.backgroundColor = 'violet';
-    fragment.appendChild(div);
+parentContainer.style.backgroundColor = 'white';
+
+function createGrid(size) {
+    let calculatedMagicalNumber = 480 / size;
+    let number = parseFloat(calculatedMagicalNumber);
+
+    parentContainer.innerHTML = '';
+
+
+    for (let i = 0; i < size * size; i++) {
+        const cell = document.createElement('div');
+        cell.style.width = `${number}px`;
+        cell.style.height = `${number}px`;
+        // cell.style.backgroundColor = 'aqua';
+        parentContainer.style.overflow = 'hidden';
+        parentContainer.appendChild(cell);
+
+    }
 }
 
 function createPixel(x, y) {
@@ -25,12 +37,13 @@ function createPixel(x, y) {
     }, 30000);
 };
 
+createGrid(16);
+
 parentContainer.addEventListener('mouseover', (e) => {
     createPixel(e.clientX, e.clientY);
     const containerChildren = parentContainer.children;
     for (let child of containerChildren) {
         child.classList.add('hover-effect');
-        // child.style.backgroundColor = 'yellow';
     }
 });
 
@@ -39,6 +52,18 @@ parentContainer.addEventListener('mouseout', () => {
     for (let child of containerChildren) {
         child.classList.remove('hover-effect');
     }
+});
+
+numberBtn.addEventListener('click', () => {
+    let inputPrompt = prompt("What's your number of squares per side? (4, 8, 16, 32, 64, etc ...)");
+    let inputNum = parseInt(inputPrompt);
+    console.log(inputNum);
+    console.log(typeof(inputNum));
+    if (isNaN(inputNum) || inputNum < 1 || inputNum > 100) {
+        alert('Please enter a number between 1 and 100.');
+        return;
+    }
+    createGrid(inputNum);
 });
 
 parentContainer.appendChild(fragment);
