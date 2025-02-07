6165d2b5ae227496c04e72936b262f69cf389383
daniel <139523036+danny-codes@users.noreply.github.com>
2025-02-07 15:01:16 -0800

    a more improved function camalize strings using .map and ternary operator

--- object-basics/script.js
+++ object-basics/script.js
@@ -296,5 +296,11 @@ function camalize(str) {
     return noDashArr.join('');
 }
 
+function betterCamalize(str) {
+    return str
+        .split('-')
+        .map((word, index) => index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1))
+        .join('');
+
 camalize('hello-world'); // "helloWorld"
 console.log(camalize('my-long-string')); // "myLongString";
