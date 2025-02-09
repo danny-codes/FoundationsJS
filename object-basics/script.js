d11fd3c72685fba7a22bc05c9eaf07159d98a5f6
daniel <139523036+danny-codes@users.noreply.github.com>
2025-02-08 21:32:02 -0800

    map to objects

--- object-basics/script.js
+++ object-basics/script.js
@@ -347,9 +347,9 @@ let sorted = copySorted(stringArr);
 
 console.log(sorted);
 
-let john = { name: "John", age: 25 };
-let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 28 };
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
 
 let userArr = [ john, pete, mary ];
 
@@ -360,3 +360,11 @@ let userArr = [ john, pete, mary ];
 let namesName = userArr.map(item => item.name);
 
 console.log(namesName);
+
+let usersMapped = userArr.map(user => ({
+    fullName: `${user.name} ${user.surname}`,
+    id: user.id
+}))
+
+console.log(usersMapped[0].id);
+console.log(usersMapped[0].fullName);
