9cd19db62ac404207849a2b4b7310c0befd53465
daniel <139523036+danny-codes@users.noreply.github.com>
2025-02-08 21:44:40 -0800

    filter unique array members

--- object-basics/script.js
+++ object-basics/script.js
@@ -392,3 +392,21 @@ function getAverageAge(users) {
 }
 
 console.log(getAverageAge(userArr));
+
+function unique(arr) {
+    let result = [];
+    
+    for (let str of arr) {
+        if (!result.includes(str)) {
+            result.push(str);
+        }
+    }
+    return result;
+}
+
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+console.log( unique(strings) );
