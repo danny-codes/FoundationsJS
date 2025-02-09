5630581b4f6b8d39fefec681c8c6a3ea8da97c42
daniel <139523036+danny-codes@users.noreply.github.com>
2025-02-08 21:50:52 -0800

    create keyed object from array

--- object-basics/script.js
+++ object-basics/script.js
@@ -410,3 +410,18 @@ let strings = ["Hare", "Krishna", "Hare", "Krishna",
 ];
 
 console.log( unique(strings) );
+
+let usersObj = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24},
+    {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+function usersObj(array) {
+    return array.reduce((obj, value) => {
+        obj[value.id] = value;
+        return obj;
+    })
+}
+
+let usersById = groupById(usersObj);
